27680dd87c59a4c78784c4e7fc16f3a918603be3
changuri <chaehyun282@naver.com>
2025-08-07 14:55:53 +0900

    favorites contents ui 추가

--- src/components/Empty.tsx
+++ src/components/Empty.tsx
@@ -4,9 +4,9 @@ interface EmptyProps {
 
 export default function Empty({ msg }: EmptyProps) {
 	return (
-		<div className="pt-20 gap-6 flex flex-col items-center">
+		<div className="gap-6 flex flex-col items-center">
 			<img src="/img/icon_book.svg" alt={msg} width={80} height={80} />
-			<p className="text-gray-500">{msg}</p>
+			<p className="text-text-secondary">{msg}</p>
 		</div>
 	);
 }
